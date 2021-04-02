fe816a010cfcd43baf6d930b0280cf122c906212
koerriva <504097978@qq.com>
2021-04-02 14:21:22 +0800

    Camera MousePick优化

--- src/main/java/com/koerriva/project002/core/game/graphic/Camera2D.java
+++ src/main/java/com/koerriva/project002/core/game/graphic/Camera2D.java
@@ -5,12 +5,16 @@ import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.joml.Vector4f;
 
+import java.util.Map;
+
 public class Camera2D {
     private final Vector3f eye = new Vector3f(0f,0f,1f);
     private final Vector3f up = new Vector3f(0f,1f,0f);
     private final Vector3f center = new Vector3f(0f);
     private final Matrix4f view;
+    private final Matrix4f invView = new Matrix4f();
     private final Matrix4f projection;
+    private final Matrix4f invProjection= new Matrix4f();
     private final Vector3f mousePosition = new Vector3f();
 
     private final Window window;
@@ -18,7 +22,9 @@ public class Camera2D {
     public Camera2D(Window window) {
         this.window = window;
         view = new Matrix4f().lookAt(eye,center,up);
+        view.invert(invView);
         projection = new Matrix4f().identity();
+        projection.invert(invProjection);
     }
 
     public final Matrix4f getViewMatrix() {
@@ -34,28 +40,20 @@ public class Camera2D {
                 .ortho(-window.size.frameBufferWidth/2f,window.size.frameBufferWidth/2f
                         ,-window.size.frameBufferHeight/2f,window.size.frameBufferHeight/2f,
                         -1f,1f);
-
-        Matrix4f invP = new Matrix4f();
-        projection.get(invP);
-
-        Matrix4f invV = new Matrix4f();
-        view.get(invV);
+        projection.invert(invProjection);
+        view.invert(invView);
 
         float nx = (float) (2*window.mouse.x/window.size.width - 1f);
         float ny = (float) (1f - 2*window.mouse.y/window.size.height);
 
         Vector4f clip = new Vector4f(nx,ny,-1f,1f);
-        Vector4f eye = invP.invert().transform(clip);
+        Vector4f eye = invProjection.transform(clip);
         eye.z = -1f;
-        eye.w = 0f;
-        Vector4f pos = invV.invert().transform(eye);
+        eye.w = 1f;
+        Vector4f pos = invView.transform(eye);
         Vector3f world = new Vector3f(pos.x,pos.y,pos.z);
 
-
         mousePosition.set(world.x,world.y, world.z);
-//        invP.mul(invV).unproject(new Vector3f((float) window.mouse.x,(float) window.mouse.y,0f),
-//                new int[]{0,0,window.size.frameBufferWidth,window.size.frameBufferHeight},
-//                mousePosition);
 
         window.mouse.wx = mousePosition.x;
         window.mouse.wy = mousePosition.y;
